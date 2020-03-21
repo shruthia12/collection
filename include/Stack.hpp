2bb4e15bd687e10a0fecd4ebea0738d485df3459
Shruthi A <shruthi.a12@gmail.com>
2020-03-21 19:58:04 +0000

    Make push and pop thread safe using lock
    Using mutex ensure only 1 thread get access to push and/or pop.

--- include/Stack.hpp
+++ include/Stack.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <mutex>
 
 template <class T>
 class Stack
@@ -7,13 +8,19 @@ class Stack
     private:
         uint32_t size_;
         std::vector<T> container_;
+        std::mutex stack_lock_;
     public:
         Stack():size_(0) {}
         uint32_t size(void) { return size_; }
         bool isEmpty(void) { return size_ == 0; }
-        void push(T item) { container_.push_back(item); size_ += 1;}
+        void push(T item) {
+            const std::lock_guard<std::mutex> lock(stack_lock_);
+            container_.push_back(item);
+            size_ += 1;
+        }
         T pop(void) {
             if (!isEmpty()) {
+                const std::lock_guard<std::mutex> lock(stack_lock_);
                 auto item = container_.back();
                 container_.pop_back();
                 size_ -= 1;
